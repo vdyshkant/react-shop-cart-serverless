31c96dc9ef8e679420cf3602f0391c7c04381536
vdyshkant <develop@fastmail.co.uk>
2021-04-03 14:12:11 +0300

    added links for products items

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default function ProductList() {
   const [products, setProducts] = React.useState([])
@@ -21,13 +22,15 @@ function ProductItem({ product }) {
   return (
     <div className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-        <img className="lg:h-96 md:h-36 w-full object-cover object-center" src={image} alt="" />
+        <Link to={`/${product.id}`}>
+          <img className="lg:h-96 md:h-36 w-full object-cover object-center" src={image} alt="" />
+        </Link>
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{category}</h2>
           <h1 className="title-font text-lg font-medium text-white mb-3">{name}</h1>
           <p className="leading-relaxed mb-3">{description}</p>
           <div className="flex items-center flex-wrap ">
-            <span className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+            <Link to={`/${product.id}`} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
               See More
               <svg
                 fill="none"
@@ -40,7 +43,7 @@ function ProductItem({ product }) {
               >
                 <path d="M5 12h14M12 5l7 7-7 7"></path>
               </svg>
-            </span>
+            </Link>
             <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pr-3 py-1 border-gray-800 font-bold">
               {price}
             </span>
