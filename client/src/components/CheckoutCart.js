cf5addb02253331e971796807e1f2b11789a9524
vdyshkant <develop@fastmail.co.uk>
2021-04-03 22:50:44 +0300

    added initial checkout functionality

--- client/src/components/CheckoutCart.js
+++ client/src/components/CheckoutCart.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React from 'react'
+import { useShoppingCart } from 'use-shopping-cart'
+import useCheckout from '../utils/useCheckout'
 
 export default function CheckoutCart() {
+  const { cartCount } = useShoppingCart()
+  const handleCheckout = useCheckout()
   return (
-    <button className="inline-flex items-center bg-green-700 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-white mt-4 md:mt-0">
+    <button
+      onClick={handleCheckout}
+      className="inline-flex items-center bg-green-700 border-0 py-1 px-3 focus:outline-none hover:bg-green-600 rounded text-white mt-4 md:mt-0"
+      disabled={!cartCount}
+    >
       Go To Checkout
       <svg
         fill="none"
@@ -16,5 +24,5 @@ export default function CheckoutCart() {
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button>
-  );
+  )
 }
