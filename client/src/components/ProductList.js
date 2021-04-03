7409d3135c2de37cd379274fa93102c9405f07b3
vdyshkant <develop@fastmail.co.uk>
2021-04-03 13:57:52 +0300

    made fetched product data visible

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -15,19 +15,17 @@ export default function ProductList() {
   })
 }
 
-function ProductItem() {
+function ProductItem({ product }) {
+  const { category, name, description, price, image } = product
+
   return (
     <div className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-        <img
-          className="lg:h-96 md:h-36 w-full object-cover object-center"
-          src="https://dummyimage.com/720x400"
-          alt=""
-        />
+        <img className="lg:h-96 md:h-36 w-full object-cover object-center" src={image} alt="" />
         <div className="p-6">
-          <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">Category</h2>
-          <h1 className="title-font text-lg font-medium text-white mb-3">Name</h1>
-          <p className="leading-relaxed mb-3">Description</p>
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{category}</h2>
+          <h1 className="title-font text-lg font-medium text-white mb-3">{name}</h1>
+          <p className="leading-relaxed mb-3">{description}</p>
           <div className="flex items-center flex-wrap ">
             <span className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
               See More
@@ -44,7 +42,7 @@ function ProductItem() {
               </svg>
             </span>
             <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-lg pr-3 py-1 border-gray-800 font-bold">
-              price
+              {price}
             </span>
           </div>
         </div>
