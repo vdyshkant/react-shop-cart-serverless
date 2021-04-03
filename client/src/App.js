022fd3bcc15b598b8abac2ff291694ed37481e21
vdyshkant <develop@fastmail.co.uk>
2021-04-03 13:27:38 +0300

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import Home from './pages/Home'
+import Product from './pages/Product'
+import Result from './pages/Result'
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/result" component={Result} />
+        <Route exact path="/:productId" component={Product} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
