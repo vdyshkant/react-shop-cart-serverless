d5ea2f6787116a918f9d1bedb8c63c2a04cdb0f7
vdyshkant <develop@fastmail.co.uk>
2021-04-03 15:16:23 +0300

    added single product api and fetching

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,7 +1,21 @@
 // @ts-nocheck
-import React from "react";
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import LoadingSpinner from '../components/LoadingSpinner'
+import axios from 'axios'
+import { useQuery } from 'react-query'
 
 export default function Product() {
+  const { productId } = useParams()
+  const { data: product, isLoading, isError, error } = useQuery(['Product', productId], () =>
+    axios(`/api/products/${productId}`).then((res) => res.data.product)
+  )
+
+  if (isLoading) return <LoadingSpinner />
+  if (isError) return <div>{error}</div>
+
+  console.log(product)
+
   return (
     <section className="text-gray-400 bg-gray-900 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
@@ -12,22 +26,16 @@ export default function Product() {
             src="https://dummyimage.com/400x400"
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              Category
-            </h2>
-            <h1 className="text-white text-3xl title-font font-medium mb-8">
-              Name
-            </h1>
+            <h2 className="text-sm title-font text-gray-500 tracking-widest">Category</h2>
+            <h1 className="text-white text-3xl title-font font-medium mb-8">Name</h1>
             <p className="leading-relaxed">Description Long</p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-800 mb-5"></div>
             <div className="flex">
-              <span className="title-font font-medium text-2xl text-white">
-                Price
-              </span>
+              <span className="title-font font-medium text-2xl text-white">Price</span>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
