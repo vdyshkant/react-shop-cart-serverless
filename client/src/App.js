33b27ca42084f9b29f36f17233f11c74c4fc0094
vdyshkant <develop@fastmail.co.uk>
2021-04-03 16:30:50 +0300

    made setting up Stripe for Client

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,27 @@ import Result from './pages/Result'
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import { loadStripe } from '@stripe/stripe-js'
+import { CartProvider } from 'use-shopping-cart'
 
 const queryClient = new QueryClient()
+
+const stripePromise = loadStripe(
+  'pk_test_51Ic98cH9oF8StYkdSyGeFNkRNg38ZUKBY6feu8VjDmRhyL7RmAwfdBlqPrXd7BOxaMGG2isIjJCUZS0cWWpRQtPg00Epo7Rd5P'
+)
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/result" component={Result} />
-          <Route exact path="/:productId" component={Product} />
-        </Switch>
-      </BrowserRouter>
+      <CartProvider mode="checkout-session" stripe={stripePromise} currency="USD">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/result" component={Result} />
+            <Route exact path="/:productId" component={Product} />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
     </QueryClientProvider>
   )
 }
