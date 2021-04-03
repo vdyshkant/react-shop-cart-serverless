0587bda72a852d75d33be10f7ab45ba850fb6160
vdyshkant <develop@fastmail.co.uk>
2021-04-03 22:06:21 +0300

    added initial cart modal to manage cart

--- client/src/components/CartSummary.js
+++ client/src/components/CartSummary.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { CartIcon } from './Icons'
 import { useShoppingCart } from 'use-shopping-cart'
+import CartModal from './CartModal'
 
 export default function CartSummary() {
   const { formattedTotalPrice, cartCount } = useShoppingCart()
+  const [isModalOpen, setModalOpen] = React.useState(false)
+
+  const toggleModal = () => setModalOpen(!isModalOpen)
 
   return (
     <>
       {console.log('01 ', formattedTotalPrice)}
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <nav onClick={toggleModal} className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <span className="mr-5 hover:text-white flex items-center">
           <CartIcon />
           <span className="ml-3">
@@ -16,6 +20,7 @@ export default function CartSummary() {
           </span>
         </span>
       </nav>
+      <CartModal isOpen={isModalOpen} toggleModal={toggleModal} />
     </>
   )
 }
