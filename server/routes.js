e487e2984345e0a4a26808117e6aff875fe4c0c0
vdyshkant <develop@fastmail.co.uk>
2021-04-03 13:50:41 +0300

    added api route for products fetching

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,12 @@
-const express = require("express");
+const express = require('express')
+const products = require('./products.json')
 
 module.exports = function getRoutes() {
-  const router = express.Router();
+  const router = express.Router()
 
-  return router;
-};
+  router.get('/products', (req, res) => {
+    res.status(200).json({ products })
+  })
+
+  return router
+}
